9313f8f7a2dd1e23aec56e93c25f69addeefac26
gut <yuta1402souja@live.jp>
2019-05-26 16:38:04 +0900

    Add React

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,1 +1,20 @@
-console.log("Hello, world!");
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Hello world!</div>
+        );
+    }
+}
+
+// D&Dのデフォルトの挙動をキャンセル
+document.ondragover = document.ondrop = (e) => {
+    e.preventDefault()
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("content")
+);
